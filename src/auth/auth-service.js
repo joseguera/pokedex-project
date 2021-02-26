2b1802a572e73f3ca05989d8edd7c76be006b318
Joseguera <joseguera@uclaextension.edu>
2021-02-26 08:14:58 -0800

    updated database

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
 
+// auth services
 const AuthService = {
     getUserWithUserName(db, username) {
         return db('users')
