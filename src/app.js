43d56569a8ad7bd29abc0d513613684305b139ac
Joseguera <joseguera@uclaextension.edu>
2021-02-26 00:46:50 -0800

    added auth router and services

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const errorHandler = require('./error-handler')
+const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 
 const app = express()
@@ -16,21 +18,10 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/users', (req, res, next) => {
-    res.send('All users')
-})
+app.use(express.json());
 
 app.use('/api/users', usersRouter);
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
