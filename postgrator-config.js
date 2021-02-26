fdb99e63eadb4fb09f0b1e0f580c9d6091856934
Joseguera <joseguera@uclaextension.edu>
2021-02-26 07:46:47 -0800

    updated database

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL,
-  "ssl": process.env.NODE_ENV === "production"
+  "connectionString": process.env.DATABASE_URL
 }
