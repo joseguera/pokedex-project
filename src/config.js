47130cf8b24a2624a21bc86d979fb38495bc1ef0
Joseguera <joseguera@uclaextension.edu>
2021-02-26 07:38:28 -0800

    updated database

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://localhost/pokedex-project',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://localhost/pokedex-project',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
