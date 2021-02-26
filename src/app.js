2a5cb82b054f50976cdbe519dbebb1392bb52fae
Joseguera <joseguera@uclaextension.edu>
2021-02-26 08:56:46 -0800

    updated server

--- src/app.js
+++ src/app.js
@@ -1,32 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./error-handler')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./error-handler');
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 
-const app = express()
+
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 
-app.get('/users', (req, res, next) => {
-    res.send('All users')
-})
-
+// api routes
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 
 app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
